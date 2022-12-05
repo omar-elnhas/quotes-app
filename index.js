0f52209e1484394c38711700589b97ed1ae03a1d
omar-elnhas <omarelnhas100@gmail.com>
2022-12-05 16:56:00 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var arrayOfQuotes = [
+    {'author': 'Jim Rohn', 
+     'quote': 'Beware of what you become in pursuit of what you want.'
+    },
+    {'author': 'Epictetus', 
+     'quote': 'It\'s not what happens to you, but how you react to it that matters.'
+    },
+    {'author': 'Frank Sinatra', 
+     'quote': 'The best revenge is massive success.'
+    },
+    {'author': 'Wayne Gretzy', 
+     'quote': 'You miss 100% of the shots you don\'t take.'
+    },
+    {'author': 'Nelson Mandela', 
+     'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+    },
+    {'author': 'Elbert Hubbard', 
+     'quote': 'Do not take life too seriously. You will not get out alive.'
+    },
+];
+
+function generateQuote(){
+    var random = Number (Math.floor((Math.random()*arrayOfQuotes.length )));
+    document.getElementById('text').textContent = `\"${arrayOfQuotes[random].quote}\"`;
+    document.getElementById('name').textContent = `--${arrayOfQuotes[random].author}`;
+    
+}
